9a7729a9e0ecf44e8b4dfe67d32d9f30fc701fb9
Narthana Epa <narthana.epa@gmail.com>
2023-07-19 23:53:43 +1000

    Refactor script

--- packer/linux/scripts/install-docker.sh
+++ packer/linux/scripts/install-docker.sh
@@ -14,10 +14,8 @@ sudo dnf install -yq \
   xz
 
 # Manual install ala https://docs.docker.com/engine/installation/binaries/
-curl -Lsf -o docker.tgz "https://download.docker.com/linux/static/${DOCKER_RELEASE}/${MACHINE}/docker-${DOCKER_VERSION}.tgz"
-tar -xvzf docker.tgz
-sudo mv docker/* /usr/bin
-rm -r docker.tgz docker
+curl -Lsf "https://download.docker.com/linux/static/${DOCKER_RELEASE}/${MACHINE}/docker-${DOCKER_VERSION}.tgz" | \
+  sudo tar -xvz -C /usr/bin --strip-components 1
 
 echo Add docker config
 sudo mkdir -p /etc/docker
@@ -51,36 +49,39 @@ sudo chmod +x /usr/local/bin/docker-*
 sudo systemctl daemon-reload
 sudo systemctl enable docker-gc.timer docker-low-disk-gc.timer
 
-echo Installing docker buildx...
 DOCKER_CLI_DIR=/usr/libexec/docker/cli-plugins
-sudo mkdir -p "${DOCKER_CLI_DIR}"
 
-echo Installing docker compose...
-DOCKER_COMPOSE_V2_ARCH="${MACHINE}"
+echo Installing docker buildx...
 case "${MACHINE}" in
   x86_64) BUILDX_ARCH="amd64";;
   aarch64) BUILDX_ARCH="arm64";;
 esac
-
-sudo curl --location --fail --silent --output "${DOCKER_CLI_DIR}/docker-buildx" "https://github.com/docker/buildx/releases/download/v${DOCKER_BUILDX_VERSION}/buildx-v${DOCKER_BUILDX_VERSION}.linux-${BUILDX_ARCH}"
+sudo mkdir -p "${DOCKER_CLI_DIR}"
+sudo curl --location --fail --silent \
+  --output "${DOCKER_CLI_DIR}/docker-buildx" \
+  "https://github.com/docker/buildx/releases/download/v${DOCKER_BUILDX_VERSION}/buildx-v${DOCKER_BUILDX_VERSION}.linux-${BUILDX_ARCH}"
 sudo chmod +x "${DOCKER_CLI_DIR}/docker-buildx"
 docker buildx version
 
-sudo curl --location --fail --silent --output "${DOCKER_CLI_DIR}/docker-compose" "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_V2_VERSION}/docker-compose-linux-${DOCKER_COMPOSE_V2_ARCH}"
+echo Installing docker compose...
+DOCKER_COMPOSE_V2_ARCH="${MACHINE}"
+sudo curl --location --fail --silent \
+  --output "${DOCKER_CLI_DIR}/docker-compose" \
+  "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_V2_VERSION}/docker-compose-linux-${DOCKER_COMPOSE_V2_ARCH}"
 sudo chmod +x "${DOCKER_CLI_DIR}/docker-compose"
 docker compose version
 
-echo "Making docker compose v2 compatible w/ docker-compose v1..."
+echo Making docker compose v2 compatible w/ docker-compose v1...
 sudo ln -s "${DOCKER_CLI_DIR}/docker-compose" /usr/bin/docker-compose
 sudo cp /tmp/conf/bin/docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
 
 # See https://docs.docker.com/build/building/multi-platform/
+echo Pull image for multiarch...
 QEMU_BINFMT_VERSION=7.0.0-28
 QEMU_BINFMT_DIGEST=sha256:66e11bea77a5ea9d6f0fe79b57cd2b189b5d15b93a2bdb925be22949232e4e55
 QEMU_BINFMT_TAG="qemu-v${QEMU_BINFMT_VERSION}@${QEMU_BINFMT_DIGEST}"
 sudo mkdir -p /usr/local/lib
 echo "QEMU_BINFMT_TAG=\"$QEMU_BINFMT_TAG\"" | sudo tee -a /usr/local/lib/bk-configure-docker.sh
-echo Pulling qemu binfmt for multiarch...
 sudo docker pull "tonistiigi/binfmt:${QEMU_BINFMT_TAG}"
